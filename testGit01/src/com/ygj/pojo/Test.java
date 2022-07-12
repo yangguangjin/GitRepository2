5a88c4ed19c207d25cc60ffc3756b66487640ecd
yangguangjin <93584387+yangguangjin@users.noreply.github.com>
2022-07-12 13:07:42 +0800

    v1.6公司测试Git环境

---
+++ testGit01/src/com/ygj/pojo/Test.java
@@ -0,0 +1,14 @@
+package com.ygj.pojo;
+
+import java.sql.SQLOutput;
+
+/**
+ * @Auther: ygj
+ * @Date: 2022/7/12 - 07 - 12 - 13:06
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("git测试");
+    }
+}
